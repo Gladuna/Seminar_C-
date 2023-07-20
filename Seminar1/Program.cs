9d3489433f6a07a82922af7644f4d23e880e11f8
Darya Gladkaya <G.Darya20@yandex.by>
2023-07-20 21:44:58 +0300

    Добавлен файл Seminar1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Seminar 1
+ /*int intNumber = 25;
+ double doubleNumber = 12.12;
+ string text  = "some text";
+ bool logicVar = true;
+
+Console.Write ("Input a number: ");
+int num = Convert.ToUInt32 (Console.ReadLine());
+
+*/
+/* Task 1/ Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.Write ("Input a first number: ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Input a second number: ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+
+int quard2 = num2 * num2;
+
+if (num1 == quard2)
+{
+    Console.WriteLine ("Yes"!);
+}
+else
+{
+    Console.WriteLine ("No!");
+}
+*/
+/* Task 2/ Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N
+Console.Write ("Input a number: ");
+int num  = Convert.ToInt32 (Console.ReadLine ());
+
+int current = -num;
+while (current <= num)
+{
+    Console.Write(current + " ");
+    current ++;
+}
+*/
