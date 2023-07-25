8d192ecfcf62a98f94abbcee84be5c8f5fae6565
Darya Gladkaya <G.Darya20@yandex.by>
2023-07-25 13:58:32 +0300

    Добавлен файл - Семинар2_дз

---
+++ Seminar2/Program.cs
@@ -0,0 +1,62 @@
+﻿// Семинар 2 - теория
+
+/* Деление нацело (уходит наименьший разряд десятков)
+
+13254 /10 = 1325
+13254/100 =132
+13254/1000 = 13
+13254/100000 = 1
+Умножение
+13254 %10 =4
+13254 %100 = 54
+13254 %1000 = 254
+13254 %10000 = 3254
+Например как извлечь число 43 из 24365 = 24365 / 100 % 100 получится 43
+*/
+/* Задача 1. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.//
+int CutNumber (int num)
+{
+    int hundreds = num / 100;
+    int units = num % 10;
+    int res = hundreds * 10 + units;
+    return res;
+}
+
+int randNumber = new Random().Next(100, 1000);
+int newNumber = CutNumber(randNumber);
+Console.WriteLine($"New version of {randNumber} is {newNumber}");
+*/
+/* Задча 2. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно a и b.
+bool IsDivisible (int num, int div1, int div2);
+{
+    if(num % div1 == 0 && num % div2 ==0)
+    return true;
+    else 
+    return false;
+} 
+//можно еще записать как return num % div1 == 0 && num % div2 ==0 без if и else//
+Console.Write("Input a number for checking: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Input a first divider: ");
+int divider1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Input a second divider: ");
+int divider2 = Convert.ToInt32 (Console.ReadLine());
+bool res = IsDivisible(num, div1, div2);
+if(res)Console.Write("Yes!");
+else Console.Write("No!");
+*/
+/* Задача 3. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+int Segment (int num)
+{int num1 = numb1 / 10;
+int num2 = num2 % 10;
+if (num1 > num2)
+    return num1;
+else
+    return num2;
+}
+
+int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+int res = (Segment);
+Console.WriteLine(num);
+*/
---
+++ Семинар2_дз/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int CutNumber (int num)
+{
+    int hundreds = num / 100;
+    int units = num % 100;
+    return units/10;
+}
+
+int randNumber = new Random().Next(100, 1000);
+int newNumber = CutNumber(randNumber);
+Console.WriteLine($"New version of {randNumber} is {newNumber}");
+*/
+/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа ( или -1, если третьей цифры нет).
+int CutNumber (int num)
+{
+    int hundreds = num / 100;
+    int units = num % 10;
+    return units;
+}
+
+Console.Write("Input a number: ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+int newNumber = CutNumber(num1);
+if (num1 < 100)
+Console.WriteLine(-1);
+else
+Console.WriteLine(newNumber);
+*/
+/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write ("Input a number: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+bool DaysMumber (int num)
+{
+    if(num >=1 && num <6)
+    return true;
+    else 
+    return false;
+}
+bool res = DaysMumber (num);
+if(res)Console.Write("Рабочий день!");
+
+bool DaysMumber1 (int num)
+{
+    if(num >=6 && num <= 7)
+    return true;
+    else 
+    return false;
+}
+bool res1 = DaysMumber1 (num);
+if(res1)Console.Write("Выходной день!");
+*/
+
+    
