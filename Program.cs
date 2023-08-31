e12abe38f03c479a1dd3ee4e5cc4b46a8be65bc1
Gladuna <139806652+Gladuna@users.noreply.github.com>
2023-08-31 14:30:17 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,51 +1,184 @@
-﻿
-
-/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-int [] CreateRandomArray (int size)
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+double[,] create_random_2d_array()
 {
-    int [] array = new int[size];
-    for (int i = 1; i < size; i++)
+    Console.Write("Input a quantity rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a quantity columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+   
+    double [,] array = new double[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
         {
-            Console.Write($"Введите число: {i}: ");
-            array[i] = Convert.ToInt32(Console.ReadLine());
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;;
         }
-    return array;
+    }
+        return array;
 }
 
-void WriteArray(int [] array)
+void show_2d_array (double[,] array)
 {
-    for (int i = 0; i < array.Length; i++)
-        Console.Write (array [i] + " ");
-    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }    
+        Console.WriteLine();
+    }
+
 }
 
-int ZeroNumbers (int [] array)
-{   int sum = 0;
-    for(int i = 0; i < array.Length; i++)
-    if (array [i] > 0)
-    sum = sum + 1;
-    return sum; 
+double [,] myArray = create_random_2d_array();
+show_2d_array(myArray);
+System.Console.WriteLine();
+*/
+
+/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и 
+// возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int[,] create_random_2d_array()
+{
+    Console.Write("Input a quantity columns: ");
+    int columns= Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a quantity rows: ");
+    int  rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    int [,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+        return array;
 }
-Console.Write ("Введите количество чисел: ");
-int m = Convert.ToInt32(Console.ReadLine());
-int [] myArray = CreateRandomArray (m);
-WriteArray(myArray);
-Console.WriteLine ($"количество чисел больше нуля " + " " + ZeroNumbers(myArray));
+void show_2d_array (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }    
+        Console.WriteLine();
+    }
+
+}
+
+void printarray (int[,] array)
+{
+    Console.Write("Введите номер строки: ");
+        int row = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите номер столбца: ");
+        int col = int.Parse(Console.ReadLine());
+
+        int numRows = array.GetLength(0);
+        int numCols = array.GetLength(1);
+
+        if (row >= 0 && row < numRows && col >= 0 && col < numCols)
+        {
+            int element = array[row, col];
+            Console.WriteLine($"Значение элемента в позиции ({row}, {col}): {element}");
+        }
+        else
+        {
+            Console.WriteLine("Указанные позиции не существуют в массиве.");
+        }
+    }
+
+int [,] myArray = create_random_2d_array();
+show_2d_array(myArray);
+printarray(myArray);
 */
 
-/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
-//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-Console.WriteLine("Введите переменную b1: ");
-double b1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите переменную k1: ");
-double k1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите переменную b2: «");
-double b2 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine ("Введите переменную k2: ");
-double k2 = Convert.ToDouble(Console.ReadLine());
 
-double x = -(b1 - b2) / (k1 - k2);
-double y = k1 * x + b1;
+/* Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+int[,] create_random_2d_array()
+{
+    Console.Write("Input a quantity columns: ");
+    int columns= Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a quantity rows: ");
+    int  rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int max = Convert.ToInt32(Console.ReadLine());
 
-Console.Write($"\n Точка пересечения двух прямых: [{x},{y}]");
+    int [,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+        return array;
+}
+void show_2d_array (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }    
+        Console.WriteLine();
+    }
+
+}
+void arithmeticMean (int[,] array)
+
+{int numRows = array.GetLength(0);
+int numCols = array.GetLength(1);
+double [] columnAverages = new double[numCols];
+
+for (int col = 0; col < numCols; col++)
+        {
+            double columnSum = 0;
+
+            for (int row = 0; row < numRows; row++)
+            {
+                columnSum += array[row, col];
+            }
+
+            columnAverages[col] = columnSum / numRows;
+        }
+
+        // Вывод средних арифметических для каждого столбца
+        Console.WriteLine("Средние арифметические для каждого столбца:");
+        for (int col = 0; col < numCols; col++)
+        {
+            Console.WriteLine($"Столбец {col + 1}: {columnAverages[col]}");
+        }
+    }
+
+int [,] myArray = create_random_2d_array();
+show_2d_array(myArray);
+arithmeticMean (myArray);
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
